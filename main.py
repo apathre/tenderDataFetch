006ef60f9742465793830b4788c1154f79f7a602
Aditya Pathre <36322026+apathre@users.noreply.github.com>
2026-04-27 19:01:59 +0530

    Update main.py
    fixed chrome driver errors version removed from line 151 in main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def init_undetected_driver(headless=True):
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--window-size=1366,768")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36")
-    driver = uc.Chrome(options=options, use_subprocess=True, version_main=144)
+    driver = uc.Chrome(options=options, use_subprocess=True)
     return driver
 
 
